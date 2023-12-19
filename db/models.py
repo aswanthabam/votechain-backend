e267ce58e446fd18a3292d78939448c89317f3c2
Aswanth Vc <aswanth.abam@gmail.com>
2023-12-19 11:15:52 +0530

    feat (model) : updated model with new contract schema

--- db/models.py
+++ db/models.py
@@ -4,9 +4,11 @@ from uuid import uuid4
 class SystemConfig(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     voterAddress = models.CharField(max_length=100)
-    electionAddress = models.CharField(max_length=100)
+    # electionAddress = models.CharField(max_length=100)
     candidateAddress = models.CharField(max_length=100)
-    adminAddress = models.CharField(max_length=100)
-    ipAddress = models.CharField(max_length=100)
-    port = models.IntegerField()
+    permissionsAddress = models.CharField(max_length=100)
+    votechainAddress = models.CharField(max_length=100)
+    # adminAddress = models.CharField(max_length=100)
+    rpcUrl = models.CharField(max_length=100)
+    wsUrl = models.CharField(max_length=100)
 
