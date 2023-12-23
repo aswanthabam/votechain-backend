b26d4201a60c8b379d87c11dc79da040a404d46e
Aswanth Vc <aswanth.abam@gmail.com>
2023-12-23 15:41:18 +0530

    feat(web3) : funder account

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("system/", include("api.system.urls")),
     path("location/", include("api.location.urls")),
+    path("web3/", include("api.web3.urls"))
 ]
