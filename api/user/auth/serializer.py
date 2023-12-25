e0cf914187ed62c7684c83337021079466031838
Aswanth Vc <aswanth.abam@gmail.com>
2023-12-25 23:49:06 +0530

    feat(userauth) : updated enc encryption from client side

--- api/user/auth/serializer.py
+++ api/user/auth/serializer.py
@@ -4,11 +4,8 @@ from utils.encryption import encrypt
 from django.conf import settings
 
 class UserAuthSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(required=True,write_only=True)
 
     def create(self, validated_data):
-        validated_data['enc2'] = encrypt(validated_data['enc2'],settings.SECRET_KEY).hex()
-        validated_data['enc1'] = encrypt(validated_data['enc1'],validated_data['password']).hex()
         obj =  UserAuth(**{
             'uid': validated_data['uid'],
             'aadhar': validated_data['aadhar'],
