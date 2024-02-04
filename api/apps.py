af4f07193fd93ac195c74808acdd07d53fc6ef34
Aswanth Vc <aswanth.abam@gmail.com>
2024-02-05 00:13:03 +0530

    fix : minor changes and vercelignore

--- api/apps.py
+++ api/apps.py
@@ -1,12 +1,12 @@
 # myapp/apps.py
 from django.apps import AppConfig
 from django.conf import settings
-if settings.DEBUG:
+if not settings.DEBUG:
     from utils.face import FaceVerifier
 class FaceConfig(AppConfig):
     name = 'api'
     def ready(self):
-        if settings.DEBUG:
+        if not settings.DEBUG:
             FaceVerifier()
 
 
