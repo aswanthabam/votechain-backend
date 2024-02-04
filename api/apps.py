3ded81b670e3f4f4e6dae242b8d30890700f0cb9
Aswanth Vc <aswanth.abam@gmail.com>
2024-02-04 23:59:55 +0530

    feat(face) : Face register multiple images and face detection using cv2

--- api/apps.py
+++ api/apps.py
@@ -1,42 +1,12 @@
 # myapp/apps.py
 from django.apps import AppConfig
 from django.conf import settings
-
+if settings.DEBUG:
+    from utils.face import FaceVerifier
 class FaceConfig(AppConfig):
-    detector = None
-    sp = None
-    facerec = None
-    FACE_THRESHOLD = 0.9
     name = 'api'
-
     def ready(self):
         if settings.DEBUG:
-            print("Loading Face Verification Models")
-            import dlib
-            FaceConfig.detector = dlib.get_frontal_face_detector()
-            FaceConfig.sp = dlib.shape_predictor("./assets/models/shape_predictor_68_face_landmarks.dat")
-            FaceConfig.facerec = dlib.face_recognition_model_v1("./assets/models/dlib_face_recognition_resnet_model_v1.dat")
-            FaceConfig.FACE_THRESHOLD = 0.9
-    @staticmethod
-    def get_face_embeddings(image_url) -> list | None:
-        if not settings.DEBUG:return None
-        import dlib
-        import numpy as np
-        image = dlib.load_rgb_image(image_url)
-        faces = FaceConfig.detector(image)
-        if len(faces) < 1:
-            return None
-        elif len(faces)> 1:
-            print("More than one face")
-            return None
-        else:
-            face = faces[0]
-            shape = FaceConfig.sp(image, face)
-            face_descriptor = FaceConfig.facerec.compute_face_descriptor(image, shape)
-            face_embedding = np.array(face_descriptor)
-            return face_embedding
-    @staticmethod
-    def verify_face(face1, face2) -> bool:
-        from scipy.spatial.distance import cosine
-        similarity = 1 - cosine(face1, face2)
-        return (similarity > FaceConfig.FACE_THRESHOLD, (similarity - 0.9) * 10)
+            FaceVerifier()
+
+
--- db/user.py
+++ db/user.py
@@ -4,20 +4,29 @@ import random, string
 
 def random_hex():
     return '0x'+(''.join([random.choice(string.ascii_letters) for x in range(46)]))
+
+class UserFace(models.Model):
+    id = models.CharField(max_length=100, primary_key=True, unique=True,default=uuid4)
+    registered = models.BooleanField(default=False)
+    face = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    face_key = models.CharField(max_length=100,null=True,blank=True, default=random_hex)
+    updated_on = models.DateTimeField(auto_now=True)
+
 class UserAuth(models.Model):
     id = models.CharField(max_length=100, primary_key=True, unique=True,default=uuid4)
     uid = models.CharField(max_length=100, unique=True, null=False, blank=False)
     aadhar = models.CharField(max_length=100, unique=True, null=False, blank=False)
     enc1 = models.CharField(max_length=200, null=False, blank=False) # encrypted mneumonics with password
     enc2 = models.CharField(max_length=200, null=False, blank=False) # encrypted mneumonic with otp
+    face = models.ForeignKey('UserFace', on_delete=models.CASCADE, related_name='user_id', null=False, blank=False,default=UserFace.objects.create)
     face_registerd = models.BooleanField(default=False)
 
-class UserFace(models.Model):
+class UserFaceCache(models.Model):
     id = models.CharField(max_length=100, primary_key=True, unique=True,default=uuid4)
-    userAuth = models.ForeignKey('UserAuth', on_delete=models.CASCADE, related_name='userAuth')
-    face = models.CharField(max_length=100, unique=True, null=False, blank=False)
-    face_key = models.CharField(max_length=100,null=False,blank=False, default=random_hex)
-    updated_on = models.DateTimeField(auto_now=True)
+    face_id = models.OneToOneField('UserFace', on_delete=models.CASCADE, related_name='face_id')
+    embeddings = models.TextField(null=False, blank=False)
+    embedding = models.TextField(null=False, blank=False)
+
     
 class UserRecovery(models.Model):
     id = models.CharField(max_length=100, primary_key=True, unique=True,default=uuid4)
