2864d661332e986949ede67315068dfce54452d6
Aswanth Vc <aswanth.abam@gmail.com>
2024-02-18 01:01:57 +0530

    fix (election) : statistics api changes

--- api/election/urls.py
+++ api/election/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from .views import ElectionStatisticsAPI
 urlpatterns = [
     path('add-vote/', ElectionStatisticsAPI.as_view(), name="add-vote"),
+    path('statistics/', ElectionStatisticsAPI.as_view(), name='statistics')
 ]
--- api/election/views.py
+++ api/election/views.py
@@ -3,20 +3,46 @@ from utils.response import CustomResponse
 from .serializers import ElectionStatisticesSerializer
 from db.election import ElectionStatistices
 from utils.security import require_app_key
+from datetime import datetime
+from datetime import timedelta
 
 class ElectionStatisticsAPI(APIView):
 
     def get(self, request):
-        electionId = request.GET.get('electionId')
-        start_time = request.GET.get('start_time')
-        end_time = request.GET.get('end_time')
-        elections = ElectionStatistices.objects.filter(election=electionId)
-        if start_time:
-            elections = elections.filter(time__gte=start_time)
-        if end_time:
-            elections = elections.filter(time__lte=end_time)
-        serializer = ElectionStatisticesSerializer(elections, many=True)
-        return CustomResponse(message="Election statistics",data=serializer.data, status=200)
+        try:
+            election = request.GET.get('election')
+            start_time = request.GET.get('start_time')
+            end_time = request.GET.get('end_time')
+            response_type = request.GET.get('type','individual') # individual or time
+            gap : str= request.GET.get('gap','hour') # hour, minute, seconds
+            if not election:
+                return CustomResponse(message="Election is required",data={}).send_failure_response(400)
+            elections = ElectionStatistices.objects.filter(election=election)
+
+            if start_time:
+                start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+                elections = elections.filter(time__gte=start_time)
+            if end_time:
+                end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
+                elections = elections.filter(time__lte=end_time)
+            if response_type == 'time':
+                start_time : datetime = start_time if start_time else ElectionStatistices.objects.filter(election=election).order_by('time').first().time
+                end_time : datetime = end_time if end_time else ElectionStatistices.objects.filter(election=election).order_by('-time').first().time
+                cur_gap = start_time
+                data = {}
+                print(gap, cur_gap, end_time)
+                while cur_gap <= end_time:
+                    print(cur_gap)
+                    next_gap = cur_gap + (timedelta(hours=1) if gap == 'hour' else (timedelta(minutes=1) if gap == 'minute' else timedelta(seconds=1)))
+                    votes = elections.filter(time__gte=cur_gap, time__lte=next_gap).count()
+                    data[cur_gap.strftime('%Y-%m-%d %H:%M:%S')] = votes
+                    cur_gap = next_gap
+                return CustomResponse(message="Election statistics",data=data).send_success_response(200)
+            serializer = ElectionStatisticesSerializer(elections, many=True)
+            return CustomResponse(message="Election statistics",data=serializer.data).send_success_response(200)
+        except Exception as e:
+            print(e)
+            return CustomResponse(message="Error occured",data=str(e)).send_failure_response(500)
     
     @require_app_key
     def post(self, request):
